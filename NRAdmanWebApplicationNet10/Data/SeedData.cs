d255eaf38e368be9126fecceb074b94a9786beeb
FlareOn <FlareOn@HENDRAWANLT>
2026-05-03 12:05:50 +0700

    mbuhlah

---
+++ NRAdmanWebApplicationNet10/Data/SeedData.cs
@@ -0,0 +1,79 @@
+using NRAdmanWebApplicationNet10.Services;
+using NRAdmanWebApplicationNet10.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace NRAdmanWebApplicationNet10.Data
+{
+    public class SeedData
+    {         
+
+        private static async Task SetUsername(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames =
+            [
+                "Administrator", "Keuangan"
+            ];
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            var adminUser = new ApplicationUser()
+            {
+                UserName = "administrator",
+                NormalizedUserName = "ADMINISTRATOR",
+                Email = "administrator@gmail.com",
+                NormalizedEmail = "ADMINISTRATOR@GMAIL.COM",
+                EmailConfirmed = true,
+                SecurityStamp = string.Empty
+            };
+
+            
+            var keuanganUser = new ApplicationUser()
+            {
+                UserName = "keuangan",
+                NormalizedUserName = "KEUANGAN",
+                Email = "keuangan@gmail.com",
+                NormalizedEmail = "KEUANGAN@GMAIL.COM",
+                EmailConfirmed = true,
+                SecurityStamp = string.Empty
+            };
+
+            var adminUserExist = await userManager.FindByEmailAsync("administrator@gmail.com");
+            if (adminUserExist == null)
+            {
+                var createAdminUser = await userManager.CreateAsync(adminUser, "Administrator123!");
+                if (createAdminUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await userManager.AddToRoleAsync(adminUser, "Administrator");
+                }
+            }
+
+            var keuanganUserExist = await userManager.FindByEmailAsync("keuangan@gmail.com");
+            if (keuanganUserExist == null)
+            {
+                var createKeuanganUser = await userManager.CreateAsync(keuanganUser, "Keuangan123#");
+                if (createKeuanganUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await userManager.AddToRoleAsync(keuanganUser, "Keuangan");
+                }
+            }
+
+        }
+
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {            
+            await SetUsername(serviceProvider);
+        }
+    }
+}
--- NRAdmanWebApplicationNet10/Program.cs
+++ NRAdmanWebApplicationNet10/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NRAdmanWebApplicationNet10.Services;
+using NRAdmanWebApplicationNet10.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,21 @@ if (!string.IsNullOrEmpty(connectionString))
         options.UseMySQL(connectionString));
 }
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+        options.SignIn.RequireConfirmedAccount = true
+    )
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+// Create a service scope to get an AspnetCoreMvcFullContext instance using DI and seed the database.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,8 +39,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapStaticAssets();
 
